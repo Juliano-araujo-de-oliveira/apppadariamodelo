18f7b1fd0de83e579a9d96311a7163acb1fbfa16
Juliano <julianoaraujo8749@gmail.com>
2026-02-17 12:56:23 -0300

    fix: forcando atualizacao do sw e escopo

--- sw.js
+++ sw.js
@@ -25,4 +25,6 @@ self.addEventListener('fetch', event => {
         return response || fetch(event.request);
       })
   );
+  self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
 });
